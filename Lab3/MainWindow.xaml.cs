a2b2feec36d388719057e517a850c3f0eeae4514
Snoppei <44434924+Snoppei@users.noreply.github.com>
2024-05-18 13:29:13 +0300

    Добавьте файлы проекта.

---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System;
+using Prism.Modularity;
+using System.Reflection.PortableExecutable;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly MainWindowViewModel DataContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void OpenButton_click(object sender, RoutedEventArgs e)
+        {
+            animalPopup.IsOpen = true;
+        }
+
+        private void VisibleButtons()
+        {
+            runButton.Visibility = Visibility.Visible;
+            stopButton.Visibility = Visibility.Visible;
+            voiceButton.Visibility = Visibility.Visible;
+            upTreeButton.Visibility = Visibility.Visible;
+            downTreeButton.Visibility = Visibility.Visible;
+        }
+
+        private void DogButton(object sender, RoutedEventArgs e)
+        {
+            animalPopup.IsOpen = false;
+            DataContext.CreateClass("lab2_6.Dog");
+            info.Text = DataContext.Data;
+            VisibleButtons();
+        }
+
+        private void PantherButton(object sender, RoutedEventArgs e)
+        {
+            animalPopup.IsOpen = false;
+
+            DataContext.CreateClass("lab2_6.Panther");
+            info.Text = DataContext.Data;
+            VisibleButtons();
+        }
+
+        private void TurtleButton(object sender, RoutedEventArgs e)
+        {
+            animalPopup.IsOpen = false;
+
+            DataContext.CreateClass("lab2_6.Turtle");
+            info.Text = DataContext.Data;
+            VisibleButtons();
+        }
+
+        private void RunButton_click(object sender, RoutedEventArgs e)
+        {
+            DataContext.Run();
+            info.Text = DataContext.Data;
+        }
+
+        private void StopButton_click(object sender, RoutedEventArgs e)
+        {
+            DataContext.Stop();
+            info.Text = DataContext.Data;
+        }
+
+        private void VoiceButton_click(object sender, RoutedEventArgs e)
+        {
+            DataContext.Voice();
+            info.Text = DataContext.Data;
+        }
+
+        private void UpButton_click(object sender, RoutedEventArgs e)
+        {
+            DataContext.Up();
+            info.Text = DataContext.Data;
+        }
+
+        private void DownButton_click(object sender, RoutedEventArgs e)
+        {
+            DataContext.Down();
+            info.Text = DataContext.Data;
+        }
+
+        private async void OpenFile(object? sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DLL files (*.dll)|*.dll";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFrom(filePath);
+                        MessageBox.Show($"DLL {filePath} uploaded.", "Download DLL", MessageBoxButton.OK, MessageBoxImage.Information);
+                        DataContext.Route = filePath;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Err DLL: {ex.Message}", "Err DLL", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"DLL not found: {filePath}", "Err DLL", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            animalButton.Visibility = Visibility.Visible;
+            info.Text = DataContext.Data;
+        }
+    }
+}
---
+++ Lab3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,144 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows;
+
+public class MainWindowViewModel : BindableBase
+{
+    private object? _classFromLab2;
+    private String _route = "";
+    public List<string> NameClass { get; set; }
+    public string Type { get; set; } = "";
+    private string _data = "Select animal";
+
+    public MainWindowViewModel()
+    {
+        NameClass = new List<string>();
+        Data = _data;
+    }
+    public String Route
+    {
+        get => _route;
+        set
+        {
+            _route = value;
+            GetListClass();
+        }
+    }
+
+    public void GetListClass()
+    {
+        NameClass = new List<string>();
+
+        string assemblyPath = _route;
+
+        Assembly assembly = Assembly.LoadFrom(assemblyPath);
+
+        Type[] types = assembly.GetTypes();
+
+        var modelsTypes = types.Where(t => t.Namespace == "lab2_6");
+        foreach (Type type in types)
+        {
+            NameClass.Add(type.FullName);
+        }
+    }
+
+    public void CreateClass(String className)
+    {
+        Assembly assembly = Assembly.LoadFrom(_route);
+
+        Type? classType = assembly.GetType(className);
+
+        var listType = typeof(List<>);
+
+        Type = classType.FullName;
+
+        if (classType != null)
+        {
+
+            switch (classType.FullName)
+            {
+                case "task2_6.Dog":
+                    _classFromLab2 = Activator.CreateInstance(type: classType);
+                    Data = $"Dog max speed = {MethodFromClass("GetMaxSpeed")}";
+                    break;
+                case "task2_6.Panther":
+                    _classFromLab2 = Activator.CreateInstance(type: classType);
+                    Data = $"Panther max speed = {MethodFromClass("GetMaxSpeed")}";
+                    break;
+                case "task2_6.Turtle":
+                    _classFromLab2 = Activator.CreateInstance(type: classType);
+                    Data = $"Turtle max speed = {MethodFromClass("GetMaxSpeed")}";
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Unselected");
+        }
+
+    }
+
+    public object MethodFromClass(string nameMethod)
+    {
+        try
+        {
+            MethodInfo method = _classFromLab2.GetType().GetMethod(nameMethod);
+
+            if (method == null)
+            {
+                throw new MissingMethodException("Select method.");
+            }
+            else
+            {
+                ParameterInfo[] methodParameters = method.GetParameters();
+                object returnValue = method.Invoke(_classFromLab2, null);
+
+                if (method.ReturnType != typeof(void))
+                {
+                    Console.WriteLine($"Method selected: {method} - {returnValue}");
+                }
+
+                return returnValue;
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Err: {ex.Message}");
+            return null;
+        }
+    }
+
+    public void Run()
+    {
+        Data = (string)MethodFromClass("Go");
+    }
+
+    public void Stop()
+    {
+        Data = (string)MethodFromClass("Stay");
+    }
+
+    public void Voice()
+    {
+        Data = (string)MethodFromClass("GetVoice");
+    }
+
+    public void Up()
+    {
+        Data = (string)MethodFromClass("Up");
+    }
+
+    public void Down()
+    {
+        Data = (string)MethodFromClass("Down");
+    }
+
+    public string Data
+    {
+        get => _data;
+        set => SetProperty(ref _data, value);
+    }
+}
